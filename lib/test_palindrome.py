a413ba46146c4579d703b62fb43ec41324b7742d
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-22 13:29:45 -0700

    Finished first step of TTD now working on function

---
+++ lib/test_palindrome.py
@@ -0,0 +1,5 @@
+from palindrome import longest_palindromic_substring
+
+def test_longest_palindromic_substring ():
+    result = longest_palindromic_substring("racecar")
+    assert result == "racecar"
