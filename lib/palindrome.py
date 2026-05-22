a3494c7efa542295c9d4ce49a5f12c1683899c82
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-22 14:42:34 -0700

    Implemented palindrome check

--- lib/palindrome.py
+++ lib/palindrome.py
@@ -4,6 +4,13 @@ def longest_palindromic_substring(s):
     """
     # Function implementation comes later
     length = len(s)
+    longest = ""
     for i in range(0, length): # i is where the substring starts
         for j in range(i, length): # j is where the substring ends
             substring = s[i:j+1]  # slice the string from start index i to end index j
+            if substring == substring[::-1]: 
+                # Keep it if it beats the previous record length
+                if len(substring) > len(longest):
+                    longest = substring
+    return longest
+          
