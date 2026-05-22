64fc379c676de8444fe8fad1d33f827d682f7025
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-22 13:43:05 -0700

    Added inner and outer loops now working on palindromic functionality

--- lib/palindrome.py
+++ lib/palindrome.py
@@ -2,4 +2,8 @@ def longest_palindromic_substring(s):
     """
     Given a string s, return the longest palindromic substring.
     """
-    pass  # Function implementation comes later
+    # Function implementation comes later
+    length = len(s)
+    for i in range(0, length):
+        for j in range(i, length):
+            
